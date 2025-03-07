00d732a912c60fc93bd0e23a72ec030e35adc460
Abhishek7068 <avi.abhishek001@gmail.com>
2025-03-07 20:50:44 +0530

    first commit

---
+++ api/index.js
@@ -0,0 +1,25 @@
+const express = require("express");
+
+const cors = require('cors')
+
+
+
+
+const app = express();
+
+app.use(express.json());
+app.use(cors());
+
+app.get ('/api/greet',  (req, res) => {
+    const name = req.query.name;
+    if (!name){
+        return res.status(400).json({ error: "Name is required." });
+    }
+    res.json({message : `Hello , ${name}! Welcome to YoungLabs.`}); 
+        
+})
+
+// app.listen(3000, () =>{
+//     console.log('running')
+// })
+module.exports = app;
