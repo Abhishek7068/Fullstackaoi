200179dd44710ffdb6767ba731f07c6eb1f73655
Abhishek7068 <avi.abhishek001@gmail.com>
2025-03-07 21:33:59 +0530

    fourth

--- api/index.js
+++ api/index.js
@@ -11,7 +11,7 @@ app.use(express.json());
 app.use(cors());
 
 app.get('/', (req,res) => {
-    res.send("Welcome to My Api");
+    res.send("Welcome to My Api . Try /api/greet or /api/greet?name=yournamwe after the base url");
 })
 
 
