d275698bc6b48158204f0b28caada54887b28489
Abhishek7068 <avi.abhishek001@gmail.com>
2025-03-07 21:44:05 +0530

    5th

--- api/index.js
+++ api/index.js
@@ -8,7 +8,7 @@ const cors = require('cors')
 const app = express();
 
 app.use(express.json());
-app.use(cors());
+app.use(cors({ origin: "*" }));
 
 app.get('/', (req,res) => {
     res.send("Welcome to My Api . Try /api/greet or /api/greet?name=yournamwe after the base url");
